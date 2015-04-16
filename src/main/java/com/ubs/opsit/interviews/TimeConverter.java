eb9cfb069ed19f9647a062fd6e0aef96ef216315
Bernhard Roth <bernhard@rothconsulting.com>
2015-04-16 10:12:53 +0200

    Changed TimeConverter to a class. Implemented TimeConverter.convertTime method Fixed BerlinClockFixture class to instantiate a TimeConverter Added various scenarios to the berln-clock.story for additional boundary checks and error checks.

--- src/main/java/com/ubs/opsit/interviews/TimeConverter.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverter.java
@@ -1,7 +1,154 @@
 package com.ubs.opsit.interviews;
 
-public interface TimeConverter {
+import java.util.StringTokenizer;
 
-    String convertTime(String aTime);
+/**
+ * Convert the time passed as a string in hh:mm:ss and output as a Berlin Clock output.
+ * For input values and expected output see test file berlin-clock.story 
+ *  
+ * @author Bernhard Roth
+ *
+ */
+public class TimeConverter {
 
+	// Output creation constants
+	private static final String[] SECONDS_DISPLAY   = {"Y", "O"};
+
+	private static final String FIVE_HOUR_DISPLAY   = "RRRROOOO";
+	private static final String ONE_HOUR_DISPLAY    = "RRRROOOO";
+	
+	private static final String FIVE_MINUTE_FILLER  = "OOOOOOOOOOO";
+	private static final String FIVE_MINUTE_DISPLAY = "YYRYYRYYRYY";
+	private static final String ONE_MINUTE_DISPLAY  = "YYYYOOOO";
+
+	/**
+	 * Converts the time string to a valid clock output or a error message.
+	 * This is not throwing an exception to allow for 
+	 * 
+	 * @param aTime the time passed as a string in hh:mm:ss 
+	 * @return a valid clock output or a error message
+	 */
+	public String convertTime(String aTime) {
+		StringBuilder output = new StringBuilder(34);
+
+		try {
+			// Parse the time according to the specific needs of the Berlin clock
+			TimeParser parser = new TimeParser(aTime);
+			
+			// Seconds output choosing even values from index zero and odd values from index 1 
+			output.append(SECONDS_DISPLAY[parser.getSeconds() % 2]).append(System.lineSeparator());
+			
+			/* Five hours output. This is a sliding window over the predefined constant.
+			 * e.g. hour=17 -> R[RRRO]OOO  or  hour=2 -> RRRR[OOOO]
+			 * The start index is at 4 - (hour / 5) length is always 4 
+			 */
+			int fiveHourBeginIndex = 4 - (parser.getHours() / 5);
+			output.append(FIVE_HOUR_DISPLAY.substring(fiveHourBeginIndex, fiveHourBeginIndex + 4))
+				.append(System.lineSeparator());
+			
+			// One hours output. This is a sliding window over the predefined constant. 
+			// The start index is at 4 - (hour % 5) length is always 4
+			int oneHourBeginIndex = 4 - (parser.getHours() % 5);
+			output.append(ONE_HOUR_DISPLAY.substring(oneHourBeginIndex, oneHourBeginIndex + 4))
+				.append(System.lineSeparator());
+			
+			// Five minutes output. This is the start sequence of the constant, padded with the filler to 11 characters.
+			// e.g. minutes=43 -> [YYRYYRYY]
+			int fiveMinuteEndIndex = parser.getMinutes() / 5;
+			output.append(FIVE_MINUTE_DISPLAY.substring(0, fiveMinuteEndIndex))
+				.append(FIVE_MINUTE_FILLER.substring(fiveMinuteEndIndex, 11))
+				.append(System.lineSeparator());
+			
+			// One minute output. This is a sliding window over the predefined constant, same as in .
+			int oneMinuteBeginIndex = 4 - (parser.getMinutes() % 5);
+			output.append(ONE_MINUTE_DISPLAY.substring(oneMinuteBeginIndex, oneMinuteBeginIndex + 4));
+		} catch (IllegalArgumentException iae) {
+			return iae.getMessage();
+		}
+		
+		return output.toString();
+    }
+
+
+	/**
+	 * Parse a time format into hour, minute, second values
+	 * 
+	 * According to the test procedure a time of 24:00:00 is valid and should be displayed.
+	 * This prevents usage of SimpleDateFormatter to parse the time so parsing is done manually.
+	 * 
+	 * The assumption is that the range lies between 00:00:00 and 24:59:59
+	 * A time of 24:59:58 switches on all lights for testing.
+	 * 
+	 * @author Bernhard Roth
+	 */
+	private class TimeParser {
+
+		// Time parsing constants
+		private static final String TIME_DELIMITER = ":"; 
+
+		private int hours;
+		private int minutes;
+		private int seconds;
+
+		/**
+		 * Instantiate the class and parse the values
+		 * @param aTime A time in the format as specified in {parse(String aTime)}
+		 * @throws IllegalArgumentException
+		 */
+		private TimeParser(String aTime) throws IllegalArgumentException {
+			parse(aTime);
+		}
+		
+		/**
+		 * Expect a string in the format hh:mm:ss and tokenize it into the member variables
+		 *    
+		 * @param aTime A time as hh:mm:ss between 00:00:00 and 24:59:59
+		 * @throws IllegalArgumentException if the format is incorrect or the values are out of bounds. 
+		 */
+		private void parse(String aTime) throws IllegalArgumentException {
+
+			// Setup the tokenizer with delimiter ':'
+			StringTokenizer tokenizer = new StringTokenizer(aTime, TIME_DELIMITER);
+			
+			// We expect exactly three tokens
+			if (tokenizer.countTokens() != 3) {
+				throw new IllegalArgumentException("ERROR: Invalid time format. Expected: hh:mm:ss - Received: "+aTime);
+			}
+
+			// Parse the tokens into the array of integers
+			try {
+				hours   = Integer.parseInt(tokenizer.nextToken());
+				minutes = Integer.parseInt(tokenizer.nextToken());
+				seconds = Integer.parseInt(tokenizer.nextToken());
+			} catch (NumberFormatException iae) {
+				throw new IllegalArgumentException("ERROR: Invalid time value. Expected: between 00:00:00 and 24:59:59 - Received: '"+aTime+"'", iae);
+			}
+
+			// Verify that the tokens are within bounds
+			if (hours < 0 || hours > 24	|| minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59) {
+				throw new IllegalArgumentException("ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: "+aTime);
+			}
+		}
+		
+		/**
+		 * @return the hours
+		 */
+		public int getHours() {
+			return hours;
+		}
+
+		/**
+		 * @return the minutes
+		 */
+		public int getMinutes() {
+			return minutes;
+		}
+
+		/**
+		 * @return the seconds
+		 */
+		public int getSeconds() {
+			return seconds;
+		}
+	}
 }
--- src/test/resources/stories/berlin-clock.story
+++ src/test/resources/stories/berlin-clock.story
@@ -17,6 +17,15 @@ OOOO
 OOOOOOOOOOO
 OOOO
 
+Scenario: Mid morning
+When the time is 09:30:01
+Then the clock should look like
+O
+ROOO
+RRRR
+YYRYYROOOOO
+OOOO
+
 Scenario: Middle of the afternoon
 When the time is 13:17:01
 Then the clock should look like
@@ -44,5 +53,122 @@ RRRR
 OOOOOOOOOOO
 OOOO
 
+!-- Added tests for boudaries
+Scenario: One minute after midnight (zero)
+When the time is 00:01:00
+Then the clock should look like
+Y
+OOOO
+OOOO
+OOOOOOOOOOO
+YOOO
 
+Scenario: One minute after midnight (24)
+When the time is 24:01:00
+Then the clock should look like
+Y
+RRRR
+RRRR
+OOOOOOOOOOO
+YOOO
+
+Scenario: Lamptest i.e. all lights on
+When the time is 24:59:58
+Then the clock should look like
+Y
+RRRR
+RRRR
+YYRYYRYYRYY
+YYYY
+
+!-- Added tests for errors
+Scenario: Missing minutes and seconds values
+When the time is 30
+Then the clock should look like
+ERROR: Invalid time format. Expected: hh:mm:ss - Received: 30
+
+Scenario: Missing seconds value
+When the time is 30:45
+Then the clock should look like
+ERROR: Invalid time format. Expected: hh:mm:ss - Received: 30:45
+
+Scenario: Incorrect delimiter
+When the time is 13;17#01
+Then the clock should look like
+ERROR: Invalid time format. Expected: hh:mm:ss - Received: 13;17#01
+
+Scenario: Hour out of bounds
+When the time is 30:00:02
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 30:00:02
+
+Scenario: Hour out of bounds by one
+When the time is -1:00:02
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: -1:00:02
+
+Scenario: Hour out of bounds by one
+When the time is 25:00:02
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 25:00:02
+
+Scenario: Minutes out of bounds
+When the time is 00:-10:07
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 00:-10:07
+
+Scenario: Minutes out of by one
+When the time is 00:-1:07
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 00:-1:07
+
+Scenario: Minutes out of by one
+When the time is 00:60:07
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 00:60:07
+
+Scenario: Seconds out of bounds
+When the time is 00:10:85
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 00:10:85
+
+Scenario: Seconds out of bounds by one
+When the time is 00:10:60
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 00:10:60
+
+Scenario: Seconds out of bounds by one
+When the time is 00:10:-1
+Then the clock should look like
+ERROR: Time out of bounds. Expected between 00:00:00 and 24:59:59 - Received: 00:10:-1
+
+Scenario: Invalid hour
+When the time is x34:00:00
+Then the clock should look like
+ERROR: Invalid time value. Expected: between 00:00:00 and 24:59:59 - Received: 'x34:00:00'
+
+Scenario: Invalid minutes
+When the time is 00:mm:00
+Then the clock should look like
+ERROR: Invalid time value. Expected: between 00:00:00 and 24:59:59 - Received: '00:mm:00'
+
+Scenario: Invalid seconds
+When the time is 00:10:bb
+Then the clock should look like
+ERROR: Invalid time value. Expected: between 00:00:00 and 24:59:59 - Received: '00:10:bb'
+
+Scenario: Invalid seconds with leading blanks
+When the time is 00:10: 20  
+Then the clock should look like
+ERROR: Invalid time value. Expected: between 00:00:00 and 24:59:59 - Received: '00:10: 20'
+
+Scenario: Invalid minutes with leading blanks
+When the time is 00: 10:20  
+Then the clock should look like
+ERROR: Invalid time value. Expected: between 00:00:00 and 24:59:59 - Received: '00: 10:20'
+
+Scenario: Invalid hours with trailling characters
+When the time is 00&:10:20  
+Then the clock should look like
+ERROR: Invalid time value. Expected: between 00:00:00 and 24:59:59 - Received: '00&:10:20'
 
